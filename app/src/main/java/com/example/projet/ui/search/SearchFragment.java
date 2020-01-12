356a62e540e01d15172a0460ef0ac59ce65e7845
Julien Haegman <skiwa@Nostromo.local>
2020-01-12 15:21:22 +0100

    Show/hide search panel

--- app/src/main/java/com/example/projet/ui/search/SearchFragment.java
+++ app/src/main/java/com/example/projet/ui/search/SearchFragment.java
@@ -10,10 +10,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
@@ -40,11 +42,14 @@ public class SearchFragment extends Fragment {
     private Button buttonChooseEvent;
     private Button buttonChooseContacts;
     private Button buttonSearch;
+    private Button hideSearchButton;
+    private Button showSearchButton;
     private TextView selectedContactsLabel;
     private TextView selectedEventLabel;
     private TextView selectedEvent;
     private boolean hasSelectedContacts = false;
     private boolean hasSelectedEvent = false;
+
     /**
      * Création de la vue du fragment
      * @param inflater
@@ -65,13 +70,14 @@ public class SearchFragment extends Fragment {
         buttonSearch = root.findViewById(R.id.search);
         selectedEvent = root.findViewById(R.id.selected_event);
         selectedEventLabel = root.findViewById(R.id.selected_event_label);
+        hideSearchButton = root.findViewById(R.id.hideSearchButton);
+        showSearchButton = root.findViewById(R.id.showSearchButton);
 
         //Cache les élements qui doivent l'être
         selectedEventLabel.setVisibility(View.GONE);
         selectedEvent.setVisibility(View.GONE);
         selectedContactsLabel.setVisibility(View.GONE);
         contactsListView.setVisibility(View.GONE);
-
 
 
         //Selectionner un contact
@@ -110,6 +116,31 @@ public class SearchFragment extends Fragment {
             public void deleteContact(int position) {
                 //Supprime le contact de la liste
                 contacts.remove(position);
+            }
+        });
+
+
+        //Retractation du volet de recherche
+        hideSearchButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View arg0){
+                ConstraintLayout searchPanel = root.findViewById(R.id.searchPanel);
+                searchPanel.setVisibility(View.GONE);
+
+                showSearchButton.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+        //Affichage du volet de recherche
+        showSearchButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View arg0){
+                ConstraintLayout searchPanel = root.findViewById(R.id.searchPanel);
+                searchPanel.setVisibility(View.VISIBLE);
+
+                showSearchButton.setVisibility(View.GONE);
+
             }
         });
 
@@ -199,4 +230,5 @@ public class SearchFragment extends Fragment {
         contactsListAdapter.setList(contactsNames);
         contactsListAdapter.notifyDataSetChanged();
     }
+
 }
